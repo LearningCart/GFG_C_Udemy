cc5ae7e13bee43f62b8e96d0c3b8e067c9f2eab6
Jatin Gandhi <gandhijatink@gmail.com>
2025-02-23 22:02:28 +0530

    Longest word in input string

---
+++ longestword.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LINE_LENGTH		(255)
+
+int SP_GetLongestWord(char *line, int length, char **longestword);
+
+int main(int argc, char **argv)
+{
+	char *pchLongestWord;
+	int   i32LongestWordLen;
+	char  *pchLine;
+	size_t  i32actualLength;
+	pchLine = malloc(LINE_LENGTH * sizeof(char));
+	
+	if(NULL == pchLine)
+	{
+		return -1;
+	}
+	
+	memset(pchLine,0,LINE_LENGTH);
+	printf("Enter the line: ");
+	getline(&pchLine, &i32actualLength, stdin);
+    printf("\nLine: %s\n",pchLine);
+	i32LongestWordLen = SP_GetLongestWord(pchLine, i32actualLength, &pchLongestWord);
+	
+	if(i32LongestWordLen > 0)
+	{
+	    pchLongestWord[i32LongestWordLen+1] = '\0';
+		printf("Longest word is : %s\nLength: %d\n", pchLongestWord, i32LongestWordLen );
+	}
+	
+}
+
+int SP_GetLongestWord(char *line, int length, char **longestword)
+{
+	int maxlength;
+	int current_len;
+	
+	if(NULL == line || 0 == length || NULL == longestword)
+	{
+		return -1;
+	}
+
+	maxlength = current_len = 0;
+	*longestword = line;
+
+	while(NULL != line && '\0' != *line)
+	{
+		if(' ' == *line || '\t' == *line)
+		{
+			if(current_len > maxlength)
+			{
+				*longestword = line - current_len;
+				maxlength = current_len;
+			}
+			current_len = 0;
+		}
+		
+		current_len ++;
+		line ++;
+	}
+	/* Boundry case single word is passed. */
+	if(0 == maxlength && current_len > 0)
+	{
+	    *longestword = line - current_len;
+	    maxlength = current_len-1; // exclude ' ' or '\t' character
+	}
+	return maxlength;
+}
